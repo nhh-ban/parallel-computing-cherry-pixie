6658545b89347385fa40c8f8ef58286d5fcb15cf
cherry-pixie <147346541+cherry-pixie@users.noreply.github.com>
2023-10-31 05:29:11 +0100

    problem 2.1-2.3 timer

--- scripts/solution3.r
+++ scripts/solution3.r
@@ -12,37 +12,6 @@ simTweedieTest <-
     )$p.value 
   } 
 
-# Add info and misc 
-# timer from lecture material 
-printTicTocLog <-
-  function() {
-    tic.log() %>%
-      unlist %>%
-      tibble(logvals = .) %>%
-      separate(logvals,
-               sep = ":",
-               into = c("Function type", "log")) %>%
-      mutate(log = str_trim(log)) %>%
-      separate(log,
-               sep = " ",
-               into = c("Seconds"),
-               extra = "drop")
-  }
-
-tic.clearlog()
-
-# Cores 
-maxcores <- 8
-Cores <- min(parallel::detectCores(), maxcores)
-
-# Cluster the cores:
-cl <- makeCluster(Cores)
-
-# Register the cluster:
-registerDoParallel(cl)
-
-# Taking the time:
-tic(paste0("M test, ", Cores, " cores"))
 
 # Assignment 2: 
 # Problem 2.3. 
@@ -50,24 +19,41 @@ tic(paste0("M test, ", Cores, " cores"))
 
 MTweedieTests <-  
   function(N,M,sig){ 
-    foreach(
-      M,
-      .combine = 'rbind',
-      .packages = c('magrittr', 'dplyr')
-    ) %dopar%
-    sum(replicate(M,simTweedieTest(N)) < sig)/M 
+    simTweedieTest <-  # it somehow could not find this function if I did not write it again
+      function(N){ 
+        t.test( 
+          rtweedie(N, mu=10000, phi=100, power=1.9), 
+          mu=10000 
+        )$p.value 
+      } 
+    
+    # Add info and misc 
+    # Cores 
+    maxcores <- 8
+    Cores <- min(parallel::detectCores(), maxcores)
+    # Cluster
+    cl <- makeCluster(Cores)
+    # Register
+    registerDoParallel(cl)
+    
+    # M tests split 
+    m_tests<- 
+      foreach(
+        m = 1:M,
+        .combine = c,
+        .packages = c('tweedie')
+      ) %dopar% {
+      simTweedieTest(M)
+      }
+    
+    # writing what to return 
+      ans <- sum(m_tests < sig)/M 
+    
+    # Stop the clusters
+    stopCluster(cl)
+    
+    return(ans)
   } 
-
-
-# Stop the clusters
-stopCluster(cl)
-
-toc(log = TRUE)
-
-# Check comparison 
-printTicTocLog() %>%
-  knitr::kable()
-
 
 
 # Assignment 3:  
@@ -86,97 +72,3 @@ for(i in 1:nrow(df)){
       sig=.05) 
 } 
 
-
-## Assignment 4 ####
-   
-# This is one way of solving it - maybe you have a better idea? 
-# First, write a function for simulating data, where the "type" 
-# argument controls the distribution. We also need to ensure 
-# that the mean "mu" is the same for both distributions. This 
-# argument will also be needed in the t-test for the null 
-# hypothesis. Therefore, if we hard code in a value here 
-# we may later have an inconsistency between the mean of the 
-# distributions and the t-test. So, we add it as an explicit 
-# argument:  
-
-
-library(magrittr)
-library(tidyverse)
-
-simDat <-
-  function(N, type, mu) {
-    if (type == "tweedie") {
-      return(rtweedie(
-        N,
-        mu = mu,
-        phi = 100,
-        power = 1.9
-      ))
-    }
-    if (type == "normal") {
-      return(rnorm(N, mean = mu))
-    }
-    else{
-      stop("invalid distribution")
-    }
-  }
-
-
-# Next, the test. Note, we use mu two places:
-# both for the data simulation and as the null.
-simTest <-
-  function(N, type, mu) {
-    t.test(simDat(N = N,
-                  type = type,
-                  mu = mu),
-           mu = mu)$p.value
-  }
-
-
-# Running many tests is almost the same as before.
-# Here the mean is hard coded in, as we're not
-# going to change it.
-MTests <-
-  function(N, M, type, sig) {
-    sum(replicate(M,
-                  simTest(
-                    N = N,
-                    type =
-                      type,
-                    mu =
-                      10000
-                  )) < sig) / M
-  }
-
-
-# We can now repeat the same analysis as before,
-# but for both the tweedie and the normal:
-df <-
-  expand.grid(
-    N = c(10, 100, 1000, 5000),
-    M = 1000,
-    type = c("tweedie", "normal"),
-    share_reject = NA
-  ) %>%
-  as_tibble()
-
-
-for (i in 1:nrow(df)) {
-  print(i)
-  df$share_reject[i] <-
-    MTests(df$N[i],
-           df$M[i],
-           df$type[i],
-           .05)
-}
-
-# As you see, with normally distributed data, N can
-# be very small and the t-test is fine. With a tweedie,
-# "large enough" can be many thousands. If we try
-# different distributions or parameterizations, we might
-# also get different results.
-df %>%
-  ggplot2::ggplot(aes(x = log(N), y = share_reject, col = type)) +
-  geom_line() +
-  geom_hline(yintercept = .05) +
-  theme_bw() 
